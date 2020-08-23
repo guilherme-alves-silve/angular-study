06bd4d093bf13791bfcb98e07e97dac373a4a48b
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2020-08-23 01:49:04 -0300

    Cria o cabeçalho da aplicação

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { CoreModule } from './core/core.module';
+import { HomeModule } from './home/home.module';
 import { PhotosModule } from './photos/photos.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
 import { ErrorsModule } from './errors/errors.module';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    CoreModule,
     HomeModule,
     BrowserModule,
     PhotosModule,
