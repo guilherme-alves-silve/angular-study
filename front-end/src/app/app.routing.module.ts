755fba45e59ae3e7fed378edd7b0a1a1b61052f6
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2020-08-23 17:14:27 -0300

    Melhora navegação com uso de router-outlet com filhos e melhora UI

--- front-end/src/app/app.routing.module.ts
+++ front-end/src/app/app.routing.module.ts
@@ -7,19 +7,26 @@ import { PhotoListResolver } from "./photos/photo-list/photo-list.resolver";
 import { NotFoundComponent } from "./errors/not-found/not-found.component";
 import { SignInComponent } from "./home/signin/signin.component";
 import { SignUpComponent } from "./home/signup/signup.component";
+import { HomeComponent } from "./home/home.component";
 import { AuthGuard } from "./core/auth/auth.guard";
 
 const routes: Routes = [
     { 
         path: '',  
-        component: SignInComponent,
+        component: HomeComponent,
         canActivate: [ 
             AuthGuard 
+        ],
+        children: [
+            {
+                path: '',
+                component: SignInComponent
+            },
+            { 
+                path: 'signup',  
+                component: SignUpComponent
+            },
         ]
-    },
-    { 
-        path: 'signup',  
-        component: SignUpComponent
     },
     { 
         path: 'user/:userName',  
