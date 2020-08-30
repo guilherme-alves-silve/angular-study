290644286bd98aa35018b1465be11ad79a50041f
Guilherme Alves Silveira <guilherme_alves_silve@hotmail.com>
2020-08-30 18:18:05 -0300

    Adiciona footer

--- front-end/src/app/core/core.module.ts
+++ front-end/src/app/core/core.module.ts
@@ -5,13 +5,16 @@ import { RouterModule } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { HTTP_INTERCEPTORS } from "@angular/common/http";
 import { HttpRequestInterceptor } from "./auth/http-request.interceptor";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
     declarations: [
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     ],
     exports: [
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     ],
     imports: [
         CommonModule,
